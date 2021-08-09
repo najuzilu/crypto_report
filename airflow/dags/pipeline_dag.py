d2c27550d8f2f5a0c79e375cbb328afa587ef6ea
najuzilu <junaluzi@gmail.com>
2021-08-09 17:59:39 +0200

    :sparkles: [NEW] sentiment check task

--- airflow/dags/pipeline_dag.py
+++ airflow/dags/pipeline_dag.py
@@ -6,6 +6,13 @@ from airflow.utils import timezone
 from cryptowatch_to_s3 import CryptowatchToS3
 from newsapi_to_s3 import NewsApiToS3
 from sentiment import DetectNewsSentiment
+from operators import SentimentQualityOperator
+from utils import MyConfigParser
+
+
+my_config = MyConfigParser()
+AWS_REGION = my_config.aws_region()
+S3_BUCKET = my_config.s3_bucket()
 
 
 default_args = {
@@ -62,6 +69,15 @@ sentiment_task = PythonOperator(
     op_kwargs={"column_name": "title"},
 )
 
+sentiment_check = SentimentQualityOperator(
+    task_id="Check_sentiment_quality",
+    dag=dag,
+    aws_credentials_id="aws_credentials",
+    region=AWS_REGION,
+    s3_bucket=S3_BUCKET,
+    s3_prefix="news-articles-sent",
+)
+
 end_operator = DummyOperator(
     task_id="Stop_execution",
     dag=dag,
@@ -69,5 +85,6 @@ end_operator = DummyOperator(
 
 start_operator >> [crypto_task, newsapi_task]
 newsapi_task >> sentiment_task
-sentiment_task >> end_operator
+sentiment_task >> sentiment_check
+sentiment_check >> end_operator
 crypto_task >> end_operator
