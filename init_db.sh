b900d614378ea4a95f636e2ea404bec5f4f56e8a
najuzilu <junaluzi@gmail.com>
2021-08-13 12:34:27 +0200

    :ok_hand: [IMPROVE] initialize postgres for local executor

--- init_db.sh
+++ init_db.sh
@@ -1,7 +1,8 @@
 #/bin/bash init_db.sh
 CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+who_am_i=$(whoami)
 brew services start postgresql
-sudo -u postgres bash -c "psql < ${CWD}/setup_db.sql"
+sudo -u $who_am_i bash -c "psql < ${CWD}/setup_db.sql"
 airflow db init
 airflow connections delete airflow_db
 airflow connections add airflow_db --conn-uri postgres://airflow:airflow@localhost:5432/airflow
