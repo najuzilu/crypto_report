4e1f2e8f052109211c60132d2f2907b16631f63d
najuzilu <junaluzi@gmail.com>
2021-08-13 12:51:37 +0200

    :bug: [FIX] setup_db path

--- init_db.sh
+++ init_db.sh
@@ -2,7 +2,7 @@
 CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 who_am_i=$(whoami)
 brew services start postgresql
-sudo -u $who_am_i bash -c "psql < ${CWD}/setup_db.sql"
+sudo -u $who_am_i bash -c "psql < ${CWD}/airflow/setup_db.sql"
 airflow db init
 airflow connections delete airflow_db
 airflow connections add airflow_db --conn-uri postgres://airflow:airflow@localhost:5432/airflow
